d7fce94b55e7924e420b8a63c823a9ae986addd5
nerohmot <horen.tom@gmail.com>
2026-04-16 14:03:38 +0200

    feat: add omg.sh and omg-setupsh scripts

---
+++ config/boards/omg.sh
@@ -0,0 +1,11 @@
+# Open Marine Gateway Board Configuration
+
+BOARD_NAME="Open Marine Gateway"
+BOARD_SINGLE="omg"
+SOA_ARCH="arm64"
+SOC="rk3588s"
+DTB="rockchip/rk3588s-rock-5a.dtb"
+U_BOOT_TARGET="radxa-cm5-rk3588"
+U_BOOT_CONFIG="rock-5a-rk3588_defconfig"
+# Hidden boot for a clean appliance feel
+KERNEL_CMDLINE="quiet splash vt.global_cursor_default=0"
