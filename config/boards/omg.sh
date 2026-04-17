7b98888a8885c847f5ed6bf549adcc70ee101c5b
nerohmot <horen.tom@gmail.com>
2026-04-17 11:23:38 +0200

    fix: uboot problem

--- config/boards/omg.sh
+++ config/boards/omg.sh
@@ -1,14 +1,9 @@
-#
-# Open Marine Gateway Board Configuration
-#
 BOARD_NAME="Open Marine Gateway"
 BOARD_SINGLE="omg"
 SOA_ARCH="arm64"
 SOC="rk3588s"
 DTB="rockchip/rk3588s-rock-5a.dtb"
-
 U_BOOT_PACKAGE="u-boot-radxa-rk3588"
 U_BOOT_TARGET="radxa-rk3588"
 U_BOOT_CONFIG="rock-5a-rk3588_defconfig"
-
 KERNEL_CMDLINE="quiet splash vt.global_cursor_default=0"
