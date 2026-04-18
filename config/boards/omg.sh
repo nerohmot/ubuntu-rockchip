54913a287f8402a263ec97cd5ce2a7f86e53864e
nerohmot <horen.tom@gmail.com>
2026-04-18 12:20:32 +0200

    switched to vscode-only & updated some stuff

--- config/boards/omg.sh
+++ config/boards/omg.sh
@@ -2,10 +2,10 @@ BOARD_NAME="Open Marine Gateway"
 BOARD_SINGLE="omg"
 SOA_ARCH="arm64"
 SOC="rk3588s"
-DTB="rockchip/rk3588s-rock-5a.dtb"
+DTB="rockchip/rk3588s-radxa-nx5-io.dtb"
 U_BOOT_PACKAGE="u-boot-radxa-rk3588"
-U_BOOT_TARGET="radxa-rk3588"
-U_BOOT_CONFIG="rock-5a-rk3588_defconfig"
+U_BOOT_TARGET="radxa-nx5-io-rk3588s"
+U_BOOT_CONFIG="radxa-nx5-io-rk3588s_defconfig"
 KERNEL_CMDLINE="quiet splash vt.global_cursor_default=0"
 # cache_bust: Sat, Apr 18, 2026  9:37:56 AM
 # cache_bust: 1776504557
