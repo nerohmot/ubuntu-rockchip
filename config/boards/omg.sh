7eca24752b6c633e2e6fa278fe36b16a2b63b33c
nerohmot <horen.tom@gmail.com>
2026-04-18 09:38:18 +0200

    del: cache

--- config/boards/omg.sh
+++ config/boards/omg.sh
@@ -7,3 +7,4 @@ U_BOOT_PACKAGE="u-boot-radxa-rk3588"
 U_BOOT_TARGET="radxa-rk3588"
 U_BOOT_CONFIG="rock-5a-rk3588_defconfig"
 KERNEL_CMDLINE="quiet splash vt.global_cursor_default=0"
+# cache_bust: Sat, Apr 18, 2026  9:37:56 AM
