71932e201c871a6558787acbf26198b7d864341f
nerohmot <horen.tom@gmail.com>
2026-04-18 11:29:47 +0200

    chore: clear cache and retry build

--- config/boards/omg.sh
+++ config/boards/omg.sh
@@ -8,3 +8,4 @@ U_BOOT_TARGET="radxa-rk3588"
 U_BOOT_CONFIG="rock-5a-rk3588_defconfig"
 KERNEL_CMDLINE="quiet splash vt.global_cursor_default=0"
 # cache_bust: Sat, Apr 18, 2026  9:37:56 AM
+# cache_bust: 1776504557
