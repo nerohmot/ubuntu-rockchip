21560959e61c5709bfa8708ca43a9c9d399dc72d
nerohmot <horen.tom@gmail.com>
2026-04-18 11:56:30 +0200

    chore: clear cache and retry build with gateway & reporting

--- config/boards/omg.sh
+++ config/boards/omg.sh
@@ -9,3 +9,4 @@ U_BOOT_CONFIG="rock-5a-rk3588_defconfig"
 KERNEL_CMDLINE="quiet splash vt.global_cursor_default=0"
 # cache_bust: Sat, Apr 18, 2026  9:37:56 AM
 # cache_bust: 1776504557
+# cache_bust: 1776506167
